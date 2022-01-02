ba2d74fdf1c5a742599f8fd749fcf949f05fe868
RemiIO Scarlet <i@remi.email>
2022-01-03 00:00:39 +0800

    init basic elements.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['pbs.twimg.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
