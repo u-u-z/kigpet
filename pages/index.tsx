e0be82c62d57603a2e87f34c9209c7f9f9c65b76
RemiIO Scarlet <i@remi.email>
2022-01-03 01:03:42 +0800

    update color

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ const Home: NextPage = () => {
         <meta name="description" content="娃娃做的娃娃网站" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <div className="flex flex-row bg-purple-800 p-24 items-center">
+      <div className="flex flex-row bg-sky-800 p-24 items-center">
         <div className="w-3/5 h-full flex flex-col justify-center">
-          <div className=' text-purple-100  '>
+          <div className=' text-sky-100  '>
             <h1 className='text-8xl '>KIG.PET</h1>
             <p className='font-thin'>Kigurumi 小站 -- 收录娃娃作品</p>
             <p className='font-thin'></p>
@@ -27,28 +27,28 @@ const Home: NextPage = () => {
           <iframe src='https://xn--r1a.website/s/s/moekig' className='rounded-lg h-96 w-full' />
         </div>
       </div>
-      <div className='h-10 bg-purple-800'></div>
-      <div className='p-12   bg-purple-200'>
-        <div className=' bg-purple-50 rounded-lg shadow-lg justify-between -mt-24 flex flex-row items-center font-thin'>
-          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-purple-100 hover:cursor-pointer border-purple-50  hover:border-purple-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-purple-800 text-2xl'>
+      <div className='h-10 bg-sky-800'></div>
+      <div className='p-12   bg-sky-200'>
+        <div className=' bg-sky-50 rounded-lg shadow-lg justify-between -mt-24 flex flex-row items-center font-thin'>
+          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-sky-100 hover:cursor-pointer border-sky-50  hover:border-sky-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-sky-800 text-2xl'>
             <p className=' hidden lg:block'>
               <Image src='/icon/archive.svg' width={40} height={40} />
             </p>
             <p className='ml-2 text-sm lg:text-2xl'>娃娃档案</p>
           </div>
-          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-purple-100 hover:cursor-pointer border-purple-50  hover:border-purple-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-purple-800 text-2xl'>
+          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-sky-100 hover:cursor-pointer border-sky-50  hover:border-sky-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-sky-800 text-2xl'>
             <p className=' hidden lg:block'>
               <Image src='/icon/newcomer.svg' width={40} height={40} />
             </p>
             <p className='ml-2 text-sm lg:text-2xl'>新娃入坑</p>
           </div>
-          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-purple-100 hover:cursor-pointer border-purple-50  hover:border-purple-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-purple-800 text-2xl'>
+          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-sky-100 hover:cursor-pointer border-sky-50  hover:border-sky-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-sky-800 text-2xl'>
             <p className=' hidden lg:block'>
               <Image src='/icon/workshop.svg' width={40} height={40} />
             </p>
             <p className='ml-2 text-sm lg:text-2xl'>商家工坊</p>
           </div>
-          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-purple-100 hover:cursor-pointer border-purple-50  hover:border-purple-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-purple-800 text-2xl'>
+          <div className='py-4 border-2   hover:border-2 rounded-lg hover:bg-sky-100 hover:cursor-pointer border-sky-50  hover:border-sky-800  m-2 px-4  lg:px-12 text-center flex flex-row justify-around items-center text-sky-800 text-2xl'>
             <p className=' hidden lg:block'>
               <Image src='/icon/handbook.svg' width={40} height={40} />
             </p>
@@ -56,14 +56,14 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className='p-12  bg-purple-200'>
-        <div className=' bg-white rounded-lg shadow-lg -mt-24 lg:-mt-20 p-12 text-purple-800'>
-          <h2 className='text-purple-800 text-2xl font-bold mb-4'>已被收录的娃娃</h2>
+      <div className='p-12  bg-sky-200'>
+        <div className=' bg-white rounded-lg shadow-lg -mt-24 lg:-mt-20 p-12 text-sky-800'>
+          <h2 className='text-sky-800 text-2xl font-bold mb-4'>已被收录的娃娃</h2>
           <div className='flex flex-row flex-wrap justify-center sm:justify-between items-center text-2xl font-thin'>
             {
               Array(8).fill(0).map((_, i) => {
-                return <div key={i} className='border-2 p-2 rounded-lg border-purple-50 w-60 flex flex-row items-center mb-4'>
-                  <Image src='/avatar/remi.jpg' width={90} height={90} className=' rounded-full border-2 border-purple-800 w-24' />
+                return <div key={i} className='border-2 p-2 rounded-lg border-sky-50 w-60 flex flex-row items-center mb-4'>
+                  <Image src='/avatar/remi.jpg' width={90} height={90} className=' rounded-full border-2 border-sky-800 w-24' />
                   <div className='ml-4 flex flex-col justify-between h-full'>
                     <p className=''> RemiIO</p>
                     <p className='text-sm underline'>@Remi_IO</p>
